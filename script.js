7db1cd70010464a788a379c09cda208ab45dd343
rafaelmorais29 <rafaelmachadomorais29@gmail.com>
2023-11-10 13:51:22 -0300

    Criação do código

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+  let palindrome = true;
+  str = str.toLowerCase().replaceAll(' ', '');
+  
+  for(let i = 0; i < str.length; i++) {
+      if(str[i] !== str[str.length - 1 - i]) {
+          palindrome = false;
+      }
+  }
+  if(palindrome){
+      return true;
+  }else{
+      return false;
+  }
 }
 
+console.log(isPalindrome());
+
+
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+  let maximumValue = arr[0];
+  let minimumValue = arr[0];
+  
+  for(let i = 1; i < arr.length; i++){
+      if(arr[i] > maximumValue){
+          maximumValue = arr[i];
+      }else if((arr[i] < minimumValue)){
+          minimumValue = arr[i];
+      }
+  }
+  let total = [minimumValue, maximumValue]
+  return total;
 }
+
+console.log(arrayMaxMin());
